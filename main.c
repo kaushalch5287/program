e30b13954df9951f033d9f4c62a8532148e2cb43
KAUSHAL CHAUHAN <93530886+kaushalch5287@users.noreply.github.com>
2021-12-11 22:54:03 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+#include <stdio.h>
+int main()
+{
+int i, end;
+ printf("Print all natural numbers from 1 to : ");
+ scanf("%d", &end);
+ i=1;
+ while(i<=end)
+ {
+ printf("%d\n", i);
+ i++;
+ }
+ return 0;
+}
+
